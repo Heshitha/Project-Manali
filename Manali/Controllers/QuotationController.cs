50030658b1af189ded8695f3883e16cd92aca0bf
Heshitha Chathuranga <heshithachathuranga@gmail.com>
2014-11-29 10:28:40 +0530

    Project Created

---
+++ Manali/Controllers/QuotationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manali.Controllers
+{
+    public class QuotationController : Controller
+    {
+        //
+        // GET: /Quotation/
+        public ActionResult Index(int QuotationID = 0)
+        {
+            return View();
+        }
+	}
+}
