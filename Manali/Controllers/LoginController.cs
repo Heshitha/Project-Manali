6018be6de55229b5421a9724388fa80974962ead
Heshitha Chathuranga <heshithachathuranga@gmail.com>
2014-12-12 09:08:38 +0530

    Last Commit

--- Manali/Controllers/LoginController.cs
+++ Manali/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,12 @@ namespace Manali.Controllers
         // GET: /Login/
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult LoginUser(UserDTO User)
+        {
+
             return View();
         }
 	}
